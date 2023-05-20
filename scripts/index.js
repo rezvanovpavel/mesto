93babe9a85060740b69c1b41a4413d6370c91a10
Pavel Rezvanov <rezvanoffpavel@yandex.ru>
2023-05-20 20:09:29 +0300

    комментарий к коммиту

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const openPopupButtonEl = document.querySelector("#open-popup-button");
+const editPopupEl = document.querySelector("#edit-popup");
+const closePopupButtonEl = document.querySelector("#close-popup-button");
+const profileTitleEl = document.querySelector(".profile-info__title");
+const profileTextEl = document.querySelector(".profile-info__text");
+const nameInputEl = document.querySelector("#name-input");
+const vocationInputEl = document.querySelector("#vocation-input");
+const editFormEl = document.querySelector("#edit-form");
+
+openPopupButtonEl.addEventListener("click", function () {
+  openPopup(editPopupEl);
+});
+
+closePopupButtonEl.addEventListener("click", function () {
+  closePopup(editPopupEl);
+});
+
+nameInputEl.value = profileTitleEl.textContent;
+vocationInputEl.value = profileTextEl.textContent;
+
+editFormEl.addEventListener("submit", function (event) {
+  event.preventDefault();
+  profileTitleEl.textContent = nameInputEl.value;
+  profileTextEl.textContent = vocationInputEl.value;
+  closePopup(editPopupEl);
+});
+
+function openPopup(popup) {
+  popup.classList.add("popup_is_opened");
+}
+
+function closePopup(popup) {
+  popup.classList.remove("popup_is_opened");
+}
