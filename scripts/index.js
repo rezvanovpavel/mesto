11943d0350eafa169657519cab78b3045befe2e6
Pavel Rezvanov <rezvanoffpavel@yandex.ru>
2023-06-18 17:42:35 +0300

    3.0 версия

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,20 @@ const profileTextEl = document.querySelector(".profile-info__text");
 const nameInputEl = document.querySelector("#name-input");
 const vocationInputEl = document.querySelector("#vocation-input");
 const editFormEl = document.querySelector("#edit-form");
+const saveButtonProfileEL = document.querySelector("#save-button-profile");
+
+
+const validation = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-button',
+  inactiveButtonClass: 'popup__save-button_inactive',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active'
+}
+
+nameInputEl.value = profileTitleEl.textContent;
+vocationInputEl.value = profileTextEl.textContent;
 
 function openPopup(popup) {
   popup.classList.add("popup_is-opened");
@@ -19,11 +33,18 @@ openPopupButtonEl.addEventListener("click", function () {
   openPopup(editPopupEl);
   nameInputEl.value = profileTitleEl.textContent;
   vocationInputEl.value = profileTextEl.textContent;
+  hideInputError (editFormEl, nameInputEl, validation);
+  hideInputError (editFormEl, vocationInputEl, validation);
+  saveButtonProfileEL.classList.remove(validation.inactiveButtonClass);
+  saveButtonProfileEL.removeAttribute('disabled');
 });
+
 
 closePopupButtonEl.forEach((button) => { 
   const popup = button.closest('.popup');
-  button.addEventListener('click', () => closePopup(popup));
+  button.addEventListener('click', () => {
+    closePopup(popup);
+  });
 });
 
 
@@ -31,6 +52,7 @@ editFormEl.addEventListener("submit", function (event) {
   event.preventDefault();
   profileTitleEl.textContent = nameInputEl.value;
   profileTextEl.textContent = vocationInputEl.value;
+  closePopup(editPopupEl);
 });
 
 const template = document.querySelector("#element-template"); 
@@ -108,10 +130,16 @@ const editPopupPlaceEl = document.querySelector("#edit-popup-place");
 const nameInputPLaceEl = document.querySelector("#name-input-place");
 const linkInputPLaceEl = document.querySelector("#link-input-place");
 const editFormPLaceEl = document.querySelector("#edit-form-place");
+const saveButtonPlaceEl = document.querySelector("#save-button-place");
 
 openPopupButtonPLaceEl.addEventListener("click", function () {
   openPopup(editPopupPlaceEl);
+  hideInputError (editFormPLaceEl, nameInputPLaceEl, validation);
+  hideInputError (editFormPLaceEl, linkInputPLaceEl, validation);
+  saveButtonPlaceEl.classList.add(validation.inactiveButtonClass);
+  saveButtonPlaceEl.setAttribute('disabled', true);
 });
+
 
 editFormPLaceEl.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -121,9 +149,25 @@ editFormPLaceEl.addEventListener("submit", function (event) {
   const newElement = createElement(values);
   elementsEl.prepend(newElement);
   form.reset()
+  closePopup(editPopupPlaceEl);
 });
 
+const allPopupEl = document.querySelectorAll(".popup");
 
+allPopupEl.forEach((popup) => { 
+  document.addEventListener('keydown', function (event) {
+    if (event.key === "Escape") {
+      closePopup(popup)
+    }
+  });
+});
 
+allPopupEl.forEach((popup) => {
+  popup.addEventListener('click', function (event) {
+    if (event.target === event.currentTarget) {
+      closePopup(popup)
+    }
+  });
+});
 
-
+enableValidation(validation);
